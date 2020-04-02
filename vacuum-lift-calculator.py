81afc77f736e0a150323565d98ad84330e1a94e3
Michael.C Ryan <spacetime.engineer@gmail.com>
2020-04-02 10:53:13 -0400

    Update vacuum-lift-calculator.py

--- vacuum-lift-calculator.py
+++ vacuum-lift-calculator.py
@@ -27,7 +27,7 @@ class SphereVacuumPod:
         print("")
 
     def get_effective_mass(self):
-        # 
+        # Not a true negative mass but it behaves exactly as if it were. Interesting from the viewpoint that mass need only be effective to be true.
         effective_negative_mass = self.relative_vacuum_density * 4 / 3 * math.pi * self.interior_radius ** 3
         # print("Effective Vacuum Mass : " + str(0.001 * negative_mass) + " kg")
         effective_mass = effective_negative_mass + self.get_vacuum_chamber_mass()		
@@ -65,7 +65,8 @@ class TetrahedronVacuumChamber:
         print("")
 
     def get_effective_mass(self):
-        # 
+
+        # Not a true negative mass but it behaves exactly as if it were. Interesting from the viewpoint that mass need only be effective to be true.
         effective_negative_mass = self.relative_vacuum_density * self.get_tetrahedron_volume(self.interior_triangle_side_length)
         # print("Effective Vacuum Mass : " + str(0.001 * negative_mass) + " kg")
         effective_mass = effective_negative_mass + self.get_vacuum_chamber_mass()		
@@ -73,6 +74,7 @@ class TetrahedronVacuumChamber:
         return effective_mass					
 
     def get_tetrahedron_volume(self, side_length):
+        # Returns tetrahedron volume.
         return side_length ** 3 / ( 6 * 2 ** ( 0.5 ))
 
     def get_vacuum_chamber_mass(self):
@@ -99,29 +101,44 @@ while (True):
     print("Cost of aluminium is 0.00691 / cm ^ 3.")
     print("")
 
+    print("Input")
+    print("=====")
     print("What shape is the vacuum chamber? sphere = s, tetrahedron = t")
     shape = input("> ")
 
     if (shape == "s"):
+
         # Units in grams, centimeters and seconds.
-        material_density = input("What is the density of the material used to construct the vacuum chamber? : ")
-        thickness = input("What is the thickness of the spherical chamber wall? : ")
-        interior_radius = input("What is the interior radius of the vacuum chamber? : ")
-        price_per_gram = input("What is the cost per gram of material? : ")
+        print("What is the density ( in grams ) of the material used to construct the vacuum chamber?")
+        material_density = input("> ")
+        print("What is the thickness ( in cm ) of the spherical chamber wall?")
+        thickness = input("> ")
+        print("What is the interior radius ( in cm ) of the vacuum chamber?")
+        interior_radius = input("> ")
+        print("What is the price per gram ( in USD ) of material?")
+        price_per_gram = input("> ")
         print("")
 
         # Creates vacuum chamber object.
         SphereVacuumPod(float(material_density), float(thickness),  float(interior_radius), float(price_per_gram))
 
     elif(shape == "t"):
+
         # Units in grams, centimeters and seconds.
-        material_density = input("What is the density of the material used to construct the vacuum chamber? : ")
-        thickness = input("What is the thickness of the tetrahedron chamber wall? : ")
-        interior_triangle_side_length = input("What is the interior side length of the tetrahedron vacuum chamber? : ")
-        price_per_gram = input("What is the cost per gram of material? : ")
+        print("What is the density ( in grams ) of the material used to construct the vacuum chamber?")
+        material_density = input("> ")
+        print("What is the thickness ( in cm ) of the tetrahedron chamber wall?")
+        thickness = input("> ")
+        print("What is the interior side length ( in cm ) of the tetrahedron vacuum chamber?")
+        interior_triangle_side_length = input("> ")
+        print("What is the price per gram ( in USD ) of material?")
+        price_per_gram = input("> ")
         print("")
 
         # Creates vacuum chamber object.
         TetrahedronVacuumChamber(float(material_density), float(thickness),  float(interior_triangle_side_length), float(price_per_gram))
     else:
         pass
+
+    print("END")
+    print("===")
